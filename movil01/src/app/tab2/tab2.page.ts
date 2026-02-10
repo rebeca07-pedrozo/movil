b8e4cdf0ae002242795cc78898266839fc409647
Rebeca Pedrozo Cueto <rebecapedrozocueto@gmail.com>
2026-02-10 16:07:39 -0500

    tres primeros

--- movil01/src/app/tab2/tab2.page.ts
+++ movil01/src/app/tab2/tab2.page.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { IonDatetime } from '@ionic/angular/standalone';
-import { IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
+import { IonFab, IonFabButton, IonFabList, IonIcon } from '@ionic/angular/standalone';
+import { IonAvatar } from '@ionic/angular/standalone';
+import { IonAccordion, IonAccordionGroup, IonItem, IonLabel } from '@ionic/angular/standalone';
+
+
+
+
 
 import { addIcons } from 'ionicons';
 import { add } from 'ionicons/icons';
@@ -10,12 +16,17 @@ import { add } from 'ionicons/icons';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: false,
+  
 })
 export class Tab2Page {
 
   constructor() {
     addIcons({ add });
+
+    
   }
   
-
 }
+export class ExampleComponent {}
+
+
